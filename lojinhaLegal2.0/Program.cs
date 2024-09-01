3988c4c11aabc711b7b8f8884eb8e9a90158f31b
mcakunz <matheus.kunz05@gmail.com>
2024-09-01 18:28:41 -0300

    program.cs e data adicionados

--- lojinhaLegal2.0/Program.cs
+++ lojinhaLegal2.0/Program.cs
@@ -1,16 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using LojinhaLegal.Data; // Apenas este namespace é necessário
+using LojinhaLegal.Data.LojinhaLegal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicione o serviço do DbContext com a string de conexão do MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+		new MySqlServerVersion(new Version(8, 0, 23)))); // Ajuste a versão conforme sua instalação
+
+// Adicione os serviços da Razor Pages
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
